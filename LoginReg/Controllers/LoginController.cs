91cf759c88b312255de443b5eb6c6d8f92306565
Talsaniya <hiraltalsaniya98@gmail.com>
2019-02-28 13:53:53 +0530

    webassignment

---
+++ LoginReg/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using LoginReg.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LoginReg.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private webEntities db = new webEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CheckValidUser(User model)
+        {
+            string result = "Fail";
+            var DataItem = db.Users.Where(x => x.Email == model.Email && x.Password == model.Password).SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["Email"] = DataItem.Email.ToString();
+                Session["Password"] = DataItem.Password.ToString();
+                result = "Success";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Welcome()
+        {
+            if (Session["Email"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
