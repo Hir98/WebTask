08e20a4b1301109e7aa074020fafad4ab198f27c
Talsaniya <hiraltalsaniya98@gmail.com>
2019-03-08 14:18:25 +0530

    Separate Business
    Separate Business

---
+++ LoginReg/Controllers/Business/IBLLReg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoginReg.Models;
+
+namespace LoginReg.Controllers.Business
+{
+    public interface IBLLReg
+    {
+        string addRecord(User details);
+    }
+
+}
